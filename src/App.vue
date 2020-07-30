aceefb4a8dda59d7d272a3f75a13d1cb97c9350f
Joseph Cross <>
2020-07-30 14:00:04 -0400

    shade slowest cell

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
                 </thead>
                 <tbody>
                     <tr v-for="(boy, bi) in boys" :key="`boy-row-${bi}`">
-                        <td v-for="(day, i) in days" :key="`time-${i}-${bi}`" class="time">
+                        <td v-for="(day, i) in days" :key="`time-${i}-${bi}`" :class="{'slowest': isSlowest(day, getTime(day, bi))}" class="time">
                             {{ getTime(day, bi) }}
                             <svg v-if="isFastest(day, getTime(day, bi))" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="medal" class="svg-inline--fa fa-medal fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M223.75 130.75L154.62 15.54A31.997 31.997 0 0 0 127.18 0H16.03C3.08 0-4.5 14.57 2.92 25.18l111.27 158.96c29.72-27.77 67.52-46.83 109.56-53.39zM495.97 0H384.82c-11.24 0-21.66 5.9-27.44 15.54l-69.13 115.21c42.04 6.56 79.84 25.62 109.56 53.38L509.08 25.18C516.5 14.57 508.92 0 495.97 0zM256 160c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm92.52 157.26l-37.93 36.96 8.97 52.22c1.6 9.36-8.26 16.51-16.65 12.09L256 393.88l-46.9 24.65c-8.4 4.45-18.25-2.74-16.65-12.09l8.97-52.22-37.93-36.96c-6.82-6.64-3.05-18.23 6.35-19.59l52.43-7.64 23.43-47.52c2.11-4.28 6.19-6.39 10.28-6.39 4.11 0 8.22 2.14 10.33 6.39l23.43 47.52 52.43 7.64c9.4 1.36 13.17 12.95 6.35 19.59z"></path></svg>
                         </td>
@@ -310,6 +310,23 @@ export default {
 
       return fastest;
     },
+    isSlowest(day, time) {
+      const date = day.split(' ')[1];
+      let slowest = true;
+
+      if ( time === '-' ) {
+        return false;
+      }
+
+      for (var i = 0; i < this.boys.length; i++) {
+        const thisBoysTime = this.toSeconds(this.boys[i].times[date]);
+        if ( thisBoysTime > this.toSeconds(time) ) {
+          slowest = false;
+        }
+      }
+
+      return slowest;
+    },
   },
 }
 </script>
@@ -433,6 +450,11 @@ $cell-width: 70px;
 
     .time {
         position: relative;
+
+        &.slowest {
+            background: rgba(247, 226, 226, 0.2);
+            border-color: transparent;
+        }
     }
 
     .time svg {
